2a111796945af37792570d39706a191169a62c12
James Daniels <james@marginleft.com>
2013-01-23 17:37:17 -0500

    Switching to using OAuth for campfire

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source "http://rubygems.org"
 gem 'rake'
 gem 'rack-test'
 gem 'sinatra'
-gem 'tinder', '~> 1.9.2'
+gem 'tinder', :git => 'git@github.com:jamesdaniels/tinder.git', :ref => '6088eb6' # Needed for the OAuth patches
--- services/campfire.rb
+++ services/campfire.rb
@@ -5,13 +5,14 @@ class Service::Campfire < Service
 	Title = 'Campfire'
 	Description = 'Use this service to broadcast updates, crash reports, unprovisioned devices, and feedback to 37Signals Campfire.'
 
-	string :subdomain, :room_name, :api_token, :required => true
+	string :subdomain, :room_name, :required => true
 	string :sound, :default => 'rimshot', :collection => %w(secret trombone crickets rimshot vuvuzela tmyk live drama yeah greatjob pushit nyan tada ohmy bueller ohyeah)
+
+  oauth :thirty_seven_signals
 
   def settings_test
     return "Sub-domain is required" if settings(:subdomain).blank?
     return "Room name is required"  if settings(:room_name).blank?
-    return "API Token is required"  if settings(:api_token).blank?
     !!room && 'Success.' || 'Invalid room name.'
   rescue Tinder::AuthenticationFailed => e
     "Invalid sub-domain or API token."
@@ -32,7 +33,7 @@ class Service::Campfire < Service
 private
 
 	def connection
-		@connection ||= Tinder::Campfire.new settings(:subdomain), :token => settings(:api_token)
+		@connection ||= Tinder::Campfire.new settings(:subdomain), :token => settings(:access_token), :oauth => true
 	end
 
 	def subject
